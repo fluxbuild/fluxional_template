f4b2e16f0d3aa95426a66bd0b706c3ff597e08fb
tj0000 <tj@fluxbuild.io>
2023-06-25 16:42:56 -0400

    update

--- {{cookiecutter.project_name}}/app.py
+++ {{cookiecutter.project_name}}/app.py
@@ -3,25 +3,7 @@ from fluxional import Fluxional
 
 
 app = FastAPI()
-fluxional = Fluxional(
-    api_app=app,
-)
-
-
-@app.get("/hello")
-def say_hello():
-    return {"message": "hello"}
-
-
-@app.get("/run-task")
-def run_task():
-    fluxional.run_quick_task(function_name="some_task", payload={"x": "x"})
-    return {"message": "Task ran."}
-
-
-@fluxional.quick_task
-def some_task(x: str):
-    print(f"Task Ran. {x}")
+fluxional = Fluxional("FluxionalStack")
 
 
 handler = fluxional.handler()
