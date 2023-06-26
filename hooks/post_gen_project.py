0b5787e4fc13737bf4df0638b9a461f383704df3
tj0000 <tj@fluxbuild.io>
2023-06-26 18:40:28 -0400

    update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,7 +15,7 @@ def move_files():
 
     try:
         # Delete original directory 
-        shutil.rmtree("../" + "fluxional_app")
+        shutil.rmtree("../" + "{{cookiecutter.project_name}}")
     except OSError as e:
         print(e)
 
