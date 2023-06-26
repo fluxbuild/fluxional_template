527943ab68883bcf0e47d58b2f3faddf23b1ddc3
tj0000 <tj@fluxbuild.io>
2023-06-26 18:56:56 -0400

    update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,17 +6,20 @@ def move_files():
     files =  os.listdir()
 
     for file in files:
-        # Check if file exists in the moving directory do not overwrite
-        if os.path.exists("../" + file):
-            print("File " + file + " already exists in the destination directory - Skipping.")
-        else:
-            shutil.move("./" + file , "../")
-
+        try:
+            # Check if file exists in the moving directory do not overwrite
+            if os.path.exists("../" + file):
+                print("File " + file + " already exists in the destination directory - Skipping.")
+            else:
+                shutil.move("./" + file , "../")
+        except:
+            print("HERE IS THE ISSUE")
 
     try:
         # Delete original directory 
         shutil.rmtree("../" + "{{cookiecutter.project_name}}")
     except OSError as e:
+        print("NO HERE IS THE ISSUE")
         print(e)
 
 if __name__ == "__main__":
